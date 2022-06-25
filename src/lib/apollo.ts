361247fdf9be51392a78735d810a3672f716b6c0
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-06-25 08:24:16 -0300

    feat: added environment variables

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o4so930j7601xigvon0y32/master',
-  cache: new InMemoryCache(),
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
+  cache: new InMemoryCache()
 });
